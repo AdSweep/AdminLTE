aaf9207b900492cd605a5ab1c9042e897dab208b
Rick Paassen <rick.paassen@outlook.com>
2020-03-24 11:53:01 +0100

    Website change (groot) TEST

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -260,12 +260,6 @@ if($auth) {
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
                     <li><a style="pointer-events:none;"><samp><?php echo gethostname(); ?></samp></a></li>
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                            <img src="img/logo.svg" class="user-image" style="border-radius: initial" sizes="160x160" alt="Pi-hole logo" />
-                            <span class="hidden-xs">Pi-hole</span>
-                        </a>
-                    </li>
                 </ul>
             </div>
         </nav>
