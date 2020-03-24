137e7cb73a665cfaade596da4d09c42d4c9a8774
Rick Paassen <rick.paassen@outlook.com>
2020-03-24 12:05:45 +0100

    index change (groot) TEST

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -270,9 +270,6 @@ if($auth) {
         <section class="sidebar">
             <!-- Sidebar user panel -->
             <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="img/logo.svg" class="img-responsive" alt="Pi-hole logo" style="display: table; table-layout: fixed; height: 67px;" />
-                </div>
                 <div class="pull-left info">
                     <p>Status</p>
                         <?php
