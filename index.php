e4741ce9f5415f6c302df10c12b1536010e00355
Rick Paassen <rick.paassen@outlook.com>
2020-05-14 12:00:51 +0200

    settings.php tab blocklists translation

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
         <!-- small box -->
         <div class="small-box bg-adsweeporange">
             <div class="inner">
-                <p>Advertenties geblokkeerd</p>
+                <p>yo yo Yoghurt</p>
                 <h3 class="statistic"><span id="ads_blocked_today">---</span></h3>
             </div>
             <div class="icon">
